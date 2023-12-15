47d919bf16460c2a70f50838fbabd93d4dce034c
Karn3113 <kkaaiinn3@gmail.com>
2023-12-15 12:36:04 +0700

    delete status

--- resources/views/borrower/send_confirmation_form.blade.php
+++ resources/views/borrower/send_confirmation_form.blade.php
@@ -4,36 +4,6 @@ borrower confirmation form
 @endsection
 @section('content')
 <section class="section Editing">
-    
-    <div class="card pt-3">
-        <div class="card-body">
-            <div class="row">
-            <div class="container-fluid align-items-center p-0 m-0">
-                <div class="d-flex justify-content-around p-0 m-0">
-                    <div class="d-flex flex-column">
-                            <div class="text-center mb-1">
-                                <button class="btn bg-primary text-white btn-sm rounded-pill" style="width: 2rem; height: 2rem" data-bs-toggle="collapse" data-bs-target="#company3" aria-expanded="false" aria-controls="company3" onclick="stepFunction(event)">
-                                    <i class="bi bi-pencil-square"></i>
-                                </button>
-                            </div>
-                            <small class="text-primary text-center text-progress-step fw-bold">แบบยืนยันการเบิกเงินกู้ยืม</small>
-                        </div>
-                        <span class="bg-secondary w-25 rounded mt-3 me-1 ms-1" style="height: 0.17rem">
-                        </span>
-                        <div class="d-flex flex-column">
-                            <div class="text-center  mb-1">
-                                <button class="btn bg-secondary text-white btn-sm rounded-pill" style="width: 2rem; height: 2rem" data-bs-toggle="collapse" data-bs-target="#company3" aria-expanded="false" aria-controls="company3" onclick="stepFunction(event)">
-                                  <i class="bi bi-file-earmark-arrow-down"></i>
-                                </button>
-                            </div>
-                            <small class="text-secondary text-center text-progress-step fw-bold">ส่งเอกสาร</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
     <div class="card">
         <div class="card-body pt-3">
             <!-- Default Tabs -->
